7573620f9928671383a6f038c3b3196d3394913e
Davi Lux <mx.davilux@gmail.com>
2022-08-19 16:30:14 -0500

    WIP: Seed data

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -1,79 +1,73 @@
-import {app, db} from './db';
+import {app, db} from './db.js'
 import {
-  ​​  GoogleAuthProvider,
-  ​​  getAuth,
-  ​​  signInWithPopup,
-  ​​  signInWithEmailAndPassword,
-  ​​  createUserWithEmailAndPassword,
-  ​​  sendPasswordResetEmail,
-  ​​  signOut,
-  ​​} from "firebase/auth";
-  ​​import {
-  ​​  query,
-  ​​  getDocs,
-  ​​  collection,
-  ​​  where,
-  ​​  addDoc,
-  ​​} from "firebase/firestore";
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from 'firebase/auth'
+import {query, getDocs, collection, where, addDoc} from 'firebase/firestore'
 
-const auth = getAuth(app);
+export const auth = getAuth(app)
 
-const googleProvider = new GoogleAuthProvider();
+const googleProvider = new GoogleAuthProvider()
 const signInWithGoogle = async () => {
   try {
-    const res = await signInWithPopup(auth, googleProvider);
-    const user = res.user;
-    const q = query(collection(db, "users"), where("uid", "==", user.uid));
-    const docs = await getDocs(q);
+    const res = await signInWithPopup(auth, googleProvider)
+    const user = res.user
+    const q = query(collection(db, 'users'), where('uid', '==', user.uid))
+    const docs = await getDocs(q)
     if (docs.docs.length === 0) {
-      await addDoc(collection(db, "users"), {
+      await addDoc(collection(db, 'users'), {
         uid: user.uid,
         name: user.displayName,
-        authProvider: "google",
+        authProvider: 'google',
         email: user.email,
-      });
+      })
     }
   } catch (err) {
-    console.error(err);
-    alert(err.message);
+    console.error(err)
+    alert(err.message)
   }
 }
 
 const logInWithEmailAndPassword = async (email, password) => {
   try {
-    await signInWithEmailAndPassword(auth, email, password);
+    await signInWithEmailAndPassword(auth, email, password)
   } catch (err) {
-    console.error(err);
-    alert(err.message);
+    console.error(err)
+    alert(err.message)
   }
-};
+}
 
 const registerWithEmailAndPassword = async (name, email, password) => {
   try {
-    const res = await createUserWithEmailAndPassword(auth, email, password);
-    const user = res.user;
-    await addDoc(collection(db, "users"), {
+    const res = await createUserWithEmailAndPassword(auth, email, password)
+    const user = res.user
+    await addDoc(collection(db, 'users'), {
       uid: user.uid,
       name,
-      authProvider: "local",
+      authProvider: 'local',
       email,
-    });
+    })
   } catch (err) {
-    console.error(err);
-    alert(err.message);
+    console.error(err)
+    alert(err.message)
   }
-};
+}
 
 const sendPasswordReset = async (email) => {
   try {
-    await sendPasswordResetEmail(auth, email);
-    alert("Password reset link sent!");
+    await sendPasswordResetEmail(auth, email)
+    alert('Password reset link sent!')
   } catch (err) {
-    console.error(err);
-    alert(err.message);
+    console.error(err)
+    alert(err.message)
   }
-};
+}
 
 const logout = () => {
-  signOut(auth);
-};
+  signOut(auth)
+}
