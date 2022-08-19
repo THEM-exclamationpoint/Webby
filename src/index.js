a72fc42778b9d640bf8275b13bce49e419adf861
Noelle Huchette <Noelle Huchette>
2022-08-19 16:00:30 -0400

    minor fix

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
+      <Router history={history}>
         <App />
       </Router>
     </Provider>
