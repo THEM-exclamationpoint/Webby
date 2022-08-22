8e754f37b7396a1c64c7fc300e456b4a0f4ca500
Eve Frances <evefrances92@icloud.com>
2022-08-22 16:58:46 -0400

    added navbar, adjusted css file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import LogIn from "./client/components/Auth";
 import WebbyRoutes from './client/components/Routes'
+import NavBar from "./client/components/Nav";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <NavBar/>
         <WebbyRoutes/>
       </header>
     </div>
