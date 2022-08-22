a4754734d510bbcc11b50e5f5a0a616dab78d35c
Davi Lux <mx.davilux@gmail.com>
2022-08-22 10:25:33 -0500

    Seed file infinite loop resolved

--- src/firebase/seed.js
+++ src/firebase/seed.js
@@ -1,4 +1,4 @@
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import {createUserWithEmailAndPassword, signOut} from 'firebase/auth'
 import {addDoc, collection} from 'firebase/firestore'
 
 import {auth} from './auth.js'
@@ -6,10 +6,10 @@ import {db} from './db.js'
 
 const userData = [
   {
-    email: 'charlotte13@webby-social.test',
+    email: 'charlotte14@webby-social.test',
     password: 'securePassword',
-    firstName: 'Char13',
-    lastName: 'Webb13',
+    firstName: 'Char14',
+    lastName: 'Webb14',
     pronouns: ['she', 'they'],
     interests: ['Climbing', 'Cooking', 'Hiking', 'Dance', 'Crochet'],
     profilePicture: 'https://i.imgur.com/dDvuTRg.png',
@@ -49,68 +49,9 @@ const seedDatabase = async function () {
     profilePicture,
     location,
   })
+
+  signOut(auth)
   return
 }
 
 seedDatabase()
-
-// const seeding = function () {
-//   // initialize index to 0 to access each user data in the seedData array
-//   let index = 0
-//   // save the interval ID returned from the setInterval method to stop the interval later
-//   const interval = setInterval(async () => {
-//     const user = newSeedData[index]
-//     // destructure the properties on the user object
-//     const {
-//       email,
-//       password,
-//       firstName,
-//       lastName,
-//       pronouns,
-//       interests,
-//       profilePicture,
-//       likes,
-//     } = user
-//     console.log(
-//       'Current user in the setInterval: ',
-//       user.firstName,
-//       user.lastName
-//     )
-//     await firebase
-//       .auth()
-//       // create a new user account associated with the specified email and password
-//       .createUserWithEmailAndPassword(email, password)
-//       .then(async (response) => {
-//         console.log('Created user in Authentication')
-//         // store the uid from the UserCredential returned from .createUserWithEmailAndPassword method
-//         const uid = response.user.uid
-//         // create the data object with the same uid as above
-//         const data = {
-//           id: uid,
-//           email,
-//           firstName,
-//           lastName,
-//           pronouns,
-//           interests,
-//           profilePicture,
-//           likes,
-//         }
-//         // create a user document reference in the users collection using uid as identifier
-//         const usersRef = firebase.firestore().collection('users').doc(uid)
-//         // set the user document data using the data object above
-//         await usersRef.set(data)
-//         console.log('Adding user to the users collection: ', data.email)
-//       })
-//       .catch((error) => console.log(error))
-//     await firebase
-//       .auth()
-//       // sign out the current user (newly created)
-//       .signOut()
-//       .then(() => console.log('Sign out successful!'))
-//       .catch((error) => console.log(error))
-//     // increment index to access the different user data
-//     index += 1
-//     // when we reach the end of the seedData array, remove the setInterval method
-//     if (index === newSeedData.length) clearInterval(interval)
-//   }, 2000)
-// }
