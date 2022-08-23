e4e1f163cfce6434d7ce7718a24f06bccbfbb85c
Davi Lux <mx.davilux@gmail.com>
2022-08-23 15:51:57 -0500

    Added test data, added route to Graph

--- src/client/components/Routes.js
+++ src/client/components/Routes.js
@@ -1,16 +1,18 @@
-import { Routes, Route} from 'react-router-dom';
-import LogIn from './Auth/Login';
-import Home from './Home';
-import Signup from './Auth/Sign-up';
+import {Routes, Route} from 'react-router-dom'
+import LogIn from './Auth/Login'
+import Home from './Home'
+import Signup from './Auth/Sign-up'
+import Graph from './Graph.js'
 
-function WebbyRoutes () {
-    return (
-        <Routes>
-        <Route exact path="/home" element={<Home/>} />  
-        <Route exact path="/" element={<LogIn/>} />
-        <Route exact path="/register" element={<Signup/>} />
-      </Routes>
-    )
+function WebbyRoutes() {
+  return (
+    <Routes>
+      <Route exact path="/home" element={<Home />} />
+      <Route exact path="/" element={<LogIn />} />
+      <Route exact path="/register" element={<Signup />} />
+      <Route exact path="/graph" element={<Graph />} />
+    </Routes>
+  )
 }
 
-export default  WebbyRoutes
+export default WebbyRoutes
