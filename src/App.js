80806703d3c9a531836e86c2336eaa4adb8acc4e
Noelle Huchette <50929232+noellehuchette@users.noreply.github.com>
2022-08-25 12:55:02 -0400

    resolved merge

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
-import "./App.css";
 import WebbyRoutes from './client/components/Routes'
-import NavBar from "./client/components/Nav";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <NavBar/>
-        <WebbyRoutes/>
       </header>
     </div>
-  );
 }
 
-export default App;
