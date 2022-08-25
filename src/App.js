cf7b44101e0c98a6c8c5d07bd20c414563b906a7
Noelle Huchette <50929232+noellehuchette@users.noreply.github.com>
2022-08-25 12:55:11 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
+import './App.css'
 import WebbyRoutes from './client/components/Routes'
+import NavBar from './client/components/Nav'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <NavBar /> */}
+        <WebbyRoutes />
       </header>
     </div>
+  )
 }
 
+export default App
