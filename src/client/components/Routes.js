d608ca748b72785428689eaa6d219fed020a4c73
davilux <103956666+davilux@users.noreply.github.com>
2022-08-25 12:02:36 -0500

    React d3 tree (#40)
    * Added test data, added route to Graph
    
    * Committing so that I can switch to a branch to test react-d3-tree
    
    * Working chart
    
    * navbar interactivity
    
    * Fixed file structure
    
    * added settings page
    
    * Graph is now circular
    
    * some minor adjustments to the files i worked on today
    
    * resolved merge
    
    * Update Routes.js
    
    * Update App.js
    
    * Update App.js
    
    Co-authored-by: Eve Frances <evefrances92@icloud.com>
    Co-authored-by: Fi Murray <104108689+lavenderfi@users.noreply.github.com>
    Co-authored-by: Noelle Huchette <50929232+noellehuchette@users.noreply.github.com>

--- src/client/components/Routes.js
+++ src/client/components/Routes.js
@@ -1,18 +1,19 @@
-import { Routes, Route} from 'react-router-dom';
-import LogIn from './Auth/Login';
-import Home from './Home';
-import Signup from './Auth/Sign-up';
-import AppSettings from './Settings';
 
-function WebbyRoutes () {
-    return (
-      <Routes>
-        <Route exact path="/home" element={<Home/>} />  
-        <Route exact path="/" element={<LogIn/>} />
-        <Route exact path="/register" element={<Signup/>} />
-        <Route exact path="/settings" element={<AppSettings/>} />
-      </Routes>
-    )
+import {Routes, Route} from 'react-router-dom'
+import LogIn from './Auth/Login'
+import Home from './Home'
+import Signup from './Auth/Sign-up'
+import Graph from './Graph/index.js'
+
+function WebbyRoutes() {
+  return (
+    <Routes>
+      <Route exact path="/home" element={<Home />} />
+      <Route exact path="/" element={<LogIn />} />
+      <Route exact path="/register" element={<Signup />} />
+      <Route exact path="/graph" element={<Graph />} />
+    </Routes>
+  )
 }
 
-export default  WebbyRoutes
+export default WebbyRoutes
