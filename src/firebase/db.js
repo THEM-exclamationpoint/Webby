f0aef3d936f5553dfbdee8450d20ead42ee757b5
Fi Murray <104108689+lavenderfi@users.noreply.github.com>
2022-09-01 17:31:31 -0400

    webby2 added (#113)

--- src/firebase/db.js
+++ src/firebase/db.js
@@ -7,15 +7,28 @@ import {getFirestore} from 'firebase/firestore'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+// Webby
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyDmFbqZb3X-E5ZigYQx4sNQM_8AXjEyAUc',
+//   authDomain: 'webster-12a69.firebaseapp.com',
+//   projectId: 'webster-12a69',
+//   storageBucket: 'webster-12a69.appspot.com',
+//   messagingSenderId: '150449082022',
+//   appId: '1:150449082022:web:993599dba14d4211a16e67',
+//   measurementId: 'G-0RMLVGPL4S',
+// }
+
+//Webby2
 const firebaseConfig = {
-  apiKey: 'AIzaSyDmFbqZb3X-E5ZigYQx4sNQM_8AXjEyAUc',
-  authDomain: 'webster-12a69.firebaseapp.com',
-  projectId: 'webster-12a69',
-  storageBucket: 'webster-12a69.appspot.com',
-  messagingSenderId: '150449082022',
-  appId: '1:150449082022:web:993599dba14d4211a16e67',
-  measurementId: 'G-0RMLVGPL4S',
-}
+  apiKey: "AIzaSyAWg2P1znzVm686OX_TlqJnmvq_Ym3K72U",
+  authDomain: "webby2-ea4e7.firebaseapp.com",
+  projectId: "webby2-ea4e7",
+  storageBucket: "webby2-ea4e7.appspot.com",
+  messagingSenderId: "537189817627",
+  appId: "1:537189817627:web:4f594041c23124168d07be",
+  measurementId: "G-MFR0JXBV3J"
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
