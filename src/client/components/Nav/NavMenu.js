80e686273065494aebd02087feb1dd74b7d15ed8
Fi Murray <104108689+lavenderfi@users.noreply.github.com>
2022-09-01 23:03:27 -0400

    can search for users and indexes made (#117)

--- src/client/components/Nav/NavMenu.js
+++ src/client/components/Nav/NavMenu.js
@@ -1,4 +1,3 @@
-import * as React from 'react'
 import Box from '@mui/material/Box'
 import Drawer from '@mui/material/Drawer'
 import List from '@mui/material/List'
@@ -6,16 +5,24 @@ import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import MenuIcon from '@mui/icons-material/Menu'
+import TextField from '@mui/material/TextField'
+import IconButton from '@mui/material/IconButton'
+import PersonSearchOutlinedIcon from '@mui/icons-material/PersonSearchOutlined'
 import {Link} from 'react-router-dom'
-
+import React, {useState} from 'react'
+import {useSelector, useDispatch} from 'react-redux'
+import {setAllUsers} from '../../store/auth/users'
 export default function TemporaryDrawer() {
-  const [state, setState] = React.useState({
+  let [search, setSearch] = useState('')
+  const [state, setState] = useState({
     top: false,
     left: false,
     bottom: false,
     right: false,
   })
-
+  let dispatch = useDispatch()
+  let users = useSelector((state) => state.users)
+  let [searched, setSearched] = useState(false)
   const toggleDrawer = (anchor, open) => (event) => {
     if (
       event.type === 'keydown' &&
@@ -26,34 +33,67 @@ export default function TemporaryDrawer() {
 
     setState({...state, [anchor]: open})
   }
+  function handleChange(e) {
+    setSearch(e.target.value)
+  }
+  async function onSearch() {
+    await dispatch(setAllUsers())
+    setSearched(true)
+  }
 
+    const filteredData = users.filter((user) => {
+      let input = search.toLowerCase()
+      return user.name.toLowerCase().includes(input)
+    })
   const list = (anchor) => (
     <Box
       sx={{width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250}}
       role="presentation"
-      onClick={toggleDrawer(anchor, false)}
-      onKeyDown={toggleDrawer(anchor, false)}>
+    >
       <List>
-        <ListItem disablePadding>
+        <ListItem disablePadding onClick={toggleDrawer(anchor, false)}>
           <ListItemButton component={Link} to="/graph">
             <ListItemText primary="Web" />
           </ListItemButton>
         </ListItem>
-        <ListItem disablePadding>
+        <ListItem disablePadding onClick={toggleDrawer(anchor, false)}>
           <ListItemButton component={Link} to="/friends">
             <ListItemText primary="Friends" />
           </ListItemButton>
         </ListItem>
-        <ListItem disablePadding>
+        <ListItem disablePadding onClick={toggleDrawer(anchor, false)}>
           <ListItemButton component={Link} to="/chatroom">
             <ListItemText primary="Messages" />
           </ListItemButton>
         </ListItem>
-        <ListItem disablePadding>
+        <ListItem disablePadding onClick={toggleDrawer(anchor, false)}>
           <ListItemButton component={Link} to="/settings">
             <ListItemText primary="Settings" />
           </ListItemButton>
         </ListItem>
+        <ListItem>
+          <TextField
+            onChange={handleChange}
+            id="outlined-basic-email"
+            label="Search"
+            variant="outlined"
+            value={search}
+          />
+         <IconButton onClick={onSearch}> <PersonSearchOutlinedIcon  /></IconButton>
+          </ListItem>
+          <ListItem>
+          {searched && (
+            <List className='searched-user'>
+              {filteredData.map((user) => {
+                return (
+                  <Link to={`/users/${user.uid}`} key={user.uid} onClick={toggleDrawer(anchor, false)}>
+                    <ListItemButton>{user.name}</ListItemButton>
+                  </Link>
+                )
+              })}
+            </List>
+          )}</ListItem>
+        
       </List>
     </Box>
   )
