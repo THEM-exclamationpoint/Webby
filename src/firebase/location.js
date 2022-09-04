c41913b862a54735963b52d01836abc230593d9b
Noelle Huchette <Noelle Huchette>
2022-09-04 14:06:37 -0400

    fixed geocoding error

--- src/firebase/location.js
+++ src/firebase/location.js
@@ -5,7 +5,6 @@ export const zipToCoordinates = async (zip, country) => {
     country.split(/[^\w]+/gm).join('%20') +
     '&format=json' +
     '&apiKey=ed77720171db4c25b8ba86ddbce978f6'
-  let options = {method: 'GET'}
   let res = await fetch(`https://api.geoapify.com/v1/geocode/search?${params}`)
   let response = await res.json()
   let results = [...response.results]
