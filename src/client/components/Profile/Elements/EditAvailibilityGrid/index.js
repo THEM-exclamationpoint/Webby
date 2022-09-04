6418c6ccc74ebbcd155efec9b926671851fd9529
Noelle Huchette <Noelle Huchette>
2022-09-03 22:23:11 -0400

    edited availability grid on user profile, added distance calculation

--- src/client/components/Profile/Elements/EditAvailibilityGrid/index.js
+++ src/client/components/Profile/Elements/EditAvailibilityGrid/index.js
@@ -93,8 +93,8 @@ const EditAvailabilityGrid = (props) => {
                     alignItems: 'center',
                     justifyContent: 'center',
                     aspectRatio: '1/1',
-                    m: 0.5,
-                    p: 0.5,
+                    m: 0.25,
+                    p: 0.25,
                   },
                 }}>
                 <Card>
